c5d1d4a2f748008a76c746fbd3d857360f03a203
Raisenku <raisenku@prysmy.com>
2022-07-10 21:27:47 +0000

    feat: :tada: v1

---
+++ src/Config.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylarele\PhpCsFixerConfig;
+
+use PhpCsFixer\Config as CsFixerConfig;
+
+class Config
+{
+    protected readonly array $rules;
+
+    public function __construct()
+    {
+        $this->rules = [
+            '@PhpCsFixer' => true,
+
+            'blank_line_before_statement' => [
+                'break',
+                'case',
+                'continue',
+                'declare',
+                'default',
+                'do',
+                'for',
+                'foreach',
+                'if',
+                'return',
+                'switch',
+                'throw',
+                'try',
+                'while',
+                'yield',
+                'yield_from',
+            ],
+
+            'class_definition' => [
+                'inline_constructor_arguments' => false,
+                'multi_line_extends_each_single_line' => true,
+                'single_item_single_line' => true,
+                'single_line' => true,
+            ],
+
+            'control_structure_continuation_position' => [
+                'position' => 'same_line',
+            ],
+
+            'declare_parentheses' => true,
+            'declare_strict_types' => true,
+            'global_namespace_import' => true,
+            'list_syntax' => true,
+            'modernize_strpos' => true,
+            'not_operator_with_successor_space' => true,
+            'ordered_interfaces' => true,
+            'ordered_traits' => true,
+            'simplified_if_return' => true,
+            'ternary_to_null_coalescing' => true,
+        ];
+    }
+
+    public function toCsFixer(): CsFixerConfig
+    {
+        return (new CsFixerConfig())
+            ->setRules($this->rules)
+            ->setRiskyAllowed(true)
+            ->setUsingCache(true);
+    }
+
+    public static function make(): self
+    {
+        return new self;
+    }
+}
